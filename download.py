6af18bcbea7df9990fbb3225fc1d7e93c84dded4
Liu Y Cheng <xsro@foxmail.com>
2023-12-09 18:16:54 +0800

    heart

--- download.py
+++ download.py
@@ -1,18 +1,29 @@
 import argparse
 import requests
-import os
+from pathlib import Path
 from time import sleep
 import concurrent.futures
 
+description="""Clone files from https://anonymous.4open.science
+run 'pip3 install requests' to install dependencies
+run 'python3 download.py --help' to show more options
+examples: 
+    python3 download.py --name 840c8c57-3c32-451e-bf12-0e20be300389
+    python3 download.py --url https://anonymous.4open.science/r/840c8c57-3c32-451e-bf12-0e20be300389/
+"""
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Clone from the https://anonymous.4open.science')
-    parser.add_argument('--dir', type=str, default='master',
+    parser.add_argument('--dir', type=str, 
                         help='save dir')
     parser.add_argument('--url', type=str,
                         help='target anonymous github link eg., https://anonymous.4open.science/r/840c8c57-3c32-451e-bf12-0e20be300389/')
-    parser.add_argument('--max-conns', type=int, default=128,
+    parser.add_argument('--name', type=str,
+                        help='target anonymous github link id eg., 840c8c57-3c32-451e-bf12-0e20be300389, if specified this url will be ignored')
+    parser.add_argument('--max-conns', type=int, default=2,
                         help='max connections number')
-    parser.add_argument("--proxy",type=str,default="none",help="proxy server")
+    parser.add_argument("--proxy",type=str,help="proxy server")
+    parser.add_argument("--skip",type=list,default=["pyc"],help="file suffix to skip download default is pyc")
     return parser.parse_args()
 
 def dict_parse(dic, pre=None):
@@ -30,13 +41,16 @@ def dict_parse(dic, pre=None):
 proxies=None
 def req_url(dl_file, max_retry=5):
     url = dl_file[0]
-    save_path = dl_file[1]
-    save_dir = os.path.dirname(save_path)
-    if not os.path.exists(save_dir) and save_dir:
-        try:
-            os.makedirs(save_dir)
-        except OSError:
-            pass
+    save_path = Path(dl_file[1])
+    save_dir = save_path.parent
+    save_dir.mkdir(exist_ok=True,parents=True)
+    if save_path.exists():
+        with open(save_path,"r") as f:
+            line=f.readline()
+            if "Please try again later." in line:
+                pass
+            else:
+                return
     
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15"
@@ -44,9 +58,13 @@ def req_url(dl_file, max_retry=5):
     for i in range(max_retry):
         try:
             r = requests.get(url, headers=headers,proxies=proxies)
+            if "Please try again later." in r.text:
+                print(r.text)
+                print("please rerun this after some time")
+                return False
             with open(save_path, "wb") as f:
                 f.write(r.content)
-            return
+            return True
         except Exception as e:
             print('file request exception (retry {}): {} - {}'.format(i, e, save_path))
             sleep(0.4)
@@ -54,51 +72,68 @@ def req_url(dl_file, max_retry=5):
 
 if __name__ == '__main__':
     args = parse_args()
-    assert args.url, '\nPlese specifipy your target anonymous github link, \n e.g:    '\
-            +'python download.py --target https://anonymous.4open.science/r/840c8c57-3c32-451e-bf12-0e20be300389/'
-    
+
     url = args.url
-    name = url.split('/')[-2]
+    name = args.name
+    if name is None:
+        segs=url.split('/')
+        for i,s in enumerate(segs):
+            if s=="r":
+                name=segs[i+1]
     max_conns = args.max_conns
-    
+    if args.dir is None:
+        output_dir=Path(name)
+    else:
+        output_dir=Path(args.dir)
+    output_dir.mkdir(exist_ok=True,parents=True)
+    skips=args.skip
     if args.proxy:
         if args.proxy.startswith("http"):
             proxies={"https":"http://"+args.proxy}
         else:
             proxies={"https":args.proxy}
-
-    print("[*] cloning project:" + name + "with proxy",proxies)
+    print(f"[*] cloning project:{name}")
+    print(f"\tproxy",proxies)
+    print(f"\toutput to dir",output_dir)
+    print(f"\tmax connections",max_conns)
     
     list_url = "https://anonymous.4open.science/api/repo/"+ name +"/files/"
     headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15"
     }
     resp = requests.get(url=list_url, headers=headers,proxies=proxies)
+    if "Please try again later." in resp.text:
+        print(resp.text)
+        exit(1)
     file_list = resp.json()
+    file_list=dict_parse(file_list)
+
 
     print("[*] downloading files:")
     
     dl_url = "https://anonymous.4open.science/api/repo/"+ name +"/file/"
     files = []
     out = []
-    for file in dict_parse(file_list):
-        file_path = os.path.join(*file[-len(file):-2]) # * operator to unpack the arguments out of a list
-        save_path = os.path.join(args.dir, file_path)
-        file_url = dl_url + file_path
-        if file_url.endswith(".pyc"):
+    for file in file_list:
+        file_path = "/".join(file[-len(file):-2]) # * operator to unpack the arguments out of a list
+        save_path = output_dir.joinpath(file_path)
+        file_url = f"https://anonymous.4open.science/api/repo/{name}/file/{file_path}"
+        if any([file_url.endswith(s) for s in skips]):
             continue
-        files.append((file_url, save_path))
+        files.append((file_url, str(save_path)))
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_conns) as executor:
         future_to_url = (executor.submit(req_url, dl_file) for dl_file in files)
         for future in concurrent.futures.as_completed(future_to_url):
             try:
                 data = future.result()
+                if data==False:
+                    executor.shutdown()
             except Exception as exc:
                 data = str(type(exc))
             finally:
                 out.append(data)
 
-                print(str(len(out)),end="\r")
+                print(f"{len(out)}/{len(files)}",end="\r")
     
-    print("[*] files saved to:" + args.dir)
+    print("[*] files saved to:" + output_dir)
